de11fc6af7ce00a687f2847ac2ad0eb3380bbcb5
Thomas Ostfeld <thomas.ostfeld@gmail.com>
2013-11-25 20:43:51 +0100

    implementing parsing of values

--- src/com/example/torverlauf/GoalDispersionView.java
+++ src/com/example/torverlauf/GoalDispersionView.java
@@ -1,5 +1,8 @@
 package com.example.torverlauf;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
@@ -13,11 +16,33 @@ public class GoalDispersionView extends View {
 	
 	private static int BOTTOM_BAR_HEIGHT;
 	private static int BOTTOM_BAR_MARGIN_TOP;
-	private static int DIVIDER_WIDTH;
-	private static float DIVIDER_MARGIN;
+	private static float SECTION_WIDTH;
 	
 	private Paint testPaint;
 	private Paint solidPaint;
+	private Paint bluePaint;
+	private Paint grayPaint;
+	
+	private float goalHeight;
+	private int highestValue;
+	
+	private int goals1_15;
+	private int goals16_30;
+	private int goals31_45;
+	private int goals45_plus;
+	private int goals46_60;
+	private int goals61_75;
+	private int goals76_90;
+	private int goals90_plus;
+	
+	private int opponent1_15;
+	private int opponent16_30;
+	private int opponent31_45;
+	private int opponent45_plus;
+	private int opponent46_60;
+	private int opponent61_75;
+	private int opponent76_90;
+	private int opponent90_plus;
 	
 	private int height = 888;
 
@@ -49,10 +74,78 @@ public class GoalDispersionView extends View {
 		solidPaint.setColor(Color.parseColor("#676767"));
 		canvas.drawRect(0, height - BOTTOM_BAR_HEIGHT, width, height, solidPaint);
 		solidPaint.setColor(Color.parseColor("#aaaaaa"));
-		for (int i = 0; i<8; i++) {
-			canvas.drawRect(i * DIVIDER_MARGIN, 0, i * DIVIDER_MARGIN + DIVIDER_WIDTH, height, solidPaint);
+
+	}
+	
+	public void setValues() {
+		
+	}
+	
+	public void setData(Map<String, Object> subnode) {
+		ArrayList<Map<String, Object>> resultlist = (ArrayList<Map<String, Object>>) subnode.get("resultlist");
+		if (resultlist != null && !resultlist.isEmpty()) {
+			Map<String, Object> result = resultlist.get(0);
+			
+			highestValue = 0;
+			
+			goals1_15 = (Integer) result.get("tore_1_15");
+			if (goals1_15 > highestValue)
+				highestValue = goals1_15;
+			goals16_30 = (Integer) result.get("tore_16_30");
+			if (goals16_30 > highestValue)
+				highestValue = goals16_30;
+			goals31_45 = (Integer) result.get("tore_31_45");
+			if (goals31_45 > highestValue)
+				highestValue = goals31_45;
+			goals45_plus = (Integer) result.get("tore_45_plus");
+			if (goals45_plus > highestValue)
+				highestValue = goals45_plus;
+			goals46_60 = (Integer) result.get("tore_46_60");
+			if (goals46_60 > highestValue)
+				highestValue = goals46_60;
+			goals61_75 = (Integer) result.get("tore_61_75");
+			if (goals61_75 > highestValue)
+				highestValue = goals61_75;
+			goals76_90 = (Integer) result.get("tore_76_90");
+			if (goals76_90 > highestValue)
+				highestValue = goals76_90;
+			goals90_plus = (Integer) result.get("tore_90_plus");
+			if (goals90_plus > highestValue)
+				highestValue = goals90_plus;
+			
+			opponent1_15 = (Integer) result.get("gegentore_1_15");
+			if (opponent1_15 > highestValue)
+				highestValue = opponent1_15;
+			opponent16_30 = (Integer) result.get("gegentore_16_30");
+			if (opponent16_30 > highestValue)
+				highestValue = opponent16_30;
+			opponent31_45 = (Integer) result.get("gegentore_31_45");
+			if (opponent31_45 > highestValue)
+				highestValue = opponent31_45;
+			opponent45_plus = (Integer) result.get("gegentore_45_plus");
+			if (opponent45_plus > highestValue)
+				highestValue = opponent45_plus;
+			opponent46_60 = (Integer) result.get("gegentore_46_60");
+			if (opponent46_60 > highestValue)
+				highestValue = opponent46_60;
+			opponent61_75 = (Integer) result.get("gegentore_61_75");
+			if (opponent61_75 > highestValue)
+				highestValue = opponent61_75;
+			opponent76_90 = (Integer) result.get("gegentore_76_90");
+			if (opponent76_90 > highestValue)
+				highestValue = opponent76_90;
+			opponent90_plus = (Integer) result.get("gegentore_90_plus");
+			if (opponent90_plus > highestValue)
+				highestValue = opponent90_plus;
+			
+			getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+				@Override
+				public void onGlobalLayout() {
+					getViewTreeObserver().removeGlobalOnLayoutListener(this);
+					goalHeight = (float) getHeight() / highestValue;
+				}
+			});
 		}
-		canvas.drawRect(width - DIVIDER_WIDTH, 0, width, height, solidPaint);
 	}
 
 	public void initPaint() {
@@ -60,20 +153,25 @@ public class GoalDispersionView extends View {
 		testPaint.setColor(Color.RED);
 		
 		solidPaint = new Paint();
+		
+		bluePaint = new Paint();
+		bluePaint.setColor(Color.parseColor("#0000bb"));
+		
+		grayPaint = new Paint();
+		grayPaint.setColor(Color.parseColor("#aaaaaa"));
 	}
 	
 	public void initValues() {
 		Resources res = getResources();
 		BOTTOM_BAR_HEIGHT = (int) res.getDimension(R.dimen.goal_dispersion_bottom_bar_height);
 		BOTTOM_BAR_MARGIN_TOP = (int) res.getDimension(R.dimen.goal_dispersion_bottom_bar_margin_top);
-		DIVIDER_WIDTH = (int) res.getDimension(R.dimen.goal_dispersion_divider_width);
 	}
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		int parentWidth = ((View) getParent()).getWidth();
 		setMeasuredDimension(height, parentWidth);
-		DIVIDER_MARGIN = (float) (height - DIVIDER_WIDTH) / 8;
+		SECTION_WIDTH = (float) height / 8;
 	}
 	
 	@Override
